1d3e8f52571a720a75eb711cc7aa736e16eec208
Mehdi <mehdi.verfaillie@hetic.net>
2019-02-04 10:35:27 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactPlayer from 'react-player'
+
+import './App.scss';
+// import videoSubtitlesUrl from './assets/full.vtt'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+/* Initialize the state of the project with default values */
+const initState = {
+    
+}
+const rootReducer = (state = initState, action) => {
+
+    return state;
+}
+
+export default rootReducer;
