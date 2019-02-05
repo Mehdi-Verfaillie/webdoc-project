fb41033061c4aed6cbf8e976e2f4a8f3eb55c3a5
Lea <lea.syhan@gmail.com>
2019-02-05 18:25:49 +0100

    Intro Form

--- src/components/App/IntroForm/IntroForm.jsx
+++ src/components/App/IntroForm/IntroForm.jsx
@@ -6,13 +6,25 @@ import './IntroForm.scss';
 class IntroForm extends Component {
     render() {
         return(
-            <div className='flex flex-column items-center justify-center h100'>
-                <h2>what does chaos theory mean to you ?</h2>
-                <div className='flex flex-column'>
-                    <input className='input' type='text' placeholder="Word chosen" required/>
-                    <Link to='/intro' className='button'>OK</Link>
-                </div>
-            </div>
+<div>
+    <span className="C letter">C</span>
+    <span className="H letter">H</span>
+    <span className="A letter">A</span>
+    <span className="O letter">O</span>
+    <span className="S letter">S</span>
+
+    <div>
+     <div className="typewriter">
+       <h1>What does the chaos theory means for you?</h1>
+     </div>
+
+     <div className="input-content">
+        <input className="input" type="text" placeholder="Word chosen" required />
+                        <Link className="link-intro" to='/intro'> <button className="button-response" type="button">OK</button></Link>
+     </div>
+
+    </div>
+</div> 
         );
     }
 }
