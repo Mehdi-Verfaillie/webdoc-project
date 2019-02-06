043e642a7e201b9a4486f0fbac78f0712068b7b1
Théodore <theodoreyip19@gmail.com>
2019-02-06 17:08:21 +0100

    feat(api): add default content

--- src/components/App/api.js
+++ src/components/App/api.js
@@ -1,8 +1,21 @@
+const defaultContent = {
+  intro: [
+    'Think of the way a steady wind blowing across sand creates all kinds of shapes.',
+    'The grains self-organise into ripples, waves and dunes. This happens, even though the grains are virtually identical.',
+    'And have no knowledge of the shapes they became part of.',
+    'But what if all those patterns were to fall into chaos ?'
+  ]
+}
+
 const getContent = async (section) => {
-  const response = await fetch(`/api/${section}`)
-  const data = await response.json()
-  const { content } = data
-  return content
+  try {
+    const response = await fetch(`/api/${section}`)
+    const data = await response.json()
+    const { content } = data
+    return content
+  } catch (e) {
+    return defaultContent[section]
+  }
 }
 
 export { getContent }
