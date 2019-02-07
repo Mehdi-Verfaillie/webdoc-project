a58e40eef35a09bd1be8c53ee269d0df8e9501f3
Marques Fabien <fabienmrqs@gmail.com>
2019-02-07 16:00:48 +0100

    add gif  pendule and background sound

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
 import dotSvg from '../../assets/dot_btn.svg'
+import BgSound from '../../assets/Ambiant-calm.mp3'
 
 /**
  * @Import Components
@@ -13,10 +14,25 @@ import Chaos from './Chaos/Chaos';
 import Pendule from './Pendule/Pendule';
 
 class App extends Component {
-  render() {
+    constructor(props) {
+        super(props);
+        this.state = { play : false};
+        this.sound = BgSound;
+        this.audio = new Audio(this.sound);
+        this.togglePlay = this.togglePlay.bind(this);
+
+    }
+
+    togglePlay(){
+        this.setState({play: !this.state.play});
+        console.log(this.audio);
+        this.state.play ? this.audio.play() : this.audio.pause();
+    }
+
+    render() {
     return (
       <div className="App">
-        <img className="dot" src={dotSvg} alt="dot"/>
+          <img className="dot" src={dotSvg} alt="dot" onClick={this.togglePlay}/>
         <Router>
           <Fragment>
             <Route path="/introForm" component={IntroForm}/>
--- src/components/App/Pendule/Pendule.jsx
+++ src/components/App/Pendule/Pendule.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './Pendule.scss';
 import gearwheel from '../../../assets/gearWheel.png';
+import gif from '../../../assets/gif.gif';
 
 class Pendule extends Component {
     render() {
         return(
             <div className="pendule-main">
                 <div className="pendule-text">Here are 5 pendulums. Each one has a slightly different initial displacement.</div>
-                <div className="pendule-obj"></div>
+                <div className="pendule-obj">
+                    <span><img className="gif" src={gif} alt="img"/></span>
+                </div>
                 <div className="pendule-text-spe">drag the pendulum</div>
 
                 <div className="gearwheel-container">
