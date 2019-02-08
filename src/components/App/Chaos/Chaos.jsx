59bc2769c27f01c8fe16e1a908fdea4034a01428
Lea <lea.syhan@gmail.com>
2019-02-08 12:01:19 +0100

    Back home

--- src/components/App/Chaos/Chaos.jsx
+++ src/components/App/Chaos/Chaos.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './Chaos.scss';
 import './bulles.css';
 import vis from 'vis';
+import {connect} from 'react-redux'
+import Link from 'react-router-dom'
 
 class Chaos extends Component {
     constructor(props) {
